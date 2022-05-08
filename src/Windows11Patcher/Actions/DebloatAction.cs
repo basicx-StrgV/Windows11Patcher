56103e04c9ad755b4d20c52d99a0e307a8986f57
Bjarne <59922570+basicx-StrgV@users.noreply.github.com>
2022-05-08 17:25:52 +0200

    WIP

---
+++ src/Windows11Patcher/Actions/DebloatAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Windows11Patcher.Actions
+{
+    public class DebloatAction : IAction
+    {
+        public bool AutoRun { get; set; }
+
+        public void Run()
+        {
+            
+        }
+    }
+}
